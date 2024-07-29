6c1899cb8974a4951de813125b816f95677769dd
Girwar-Sahu <girvar786sahu@gmail.com>
2024-07-29 13:40:00 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const HomePage = lazy(() => import("./pages/Home"));
+const AboutPage = lazy(() => import("./pages/About"));
+const DashboardPage = lazy(() => import("./pages/Dashboard"));
+const ProjectPage = lazy(() => import("./pages/Projects"));
+const SignInPage = lazy(() => import("./pages/SignIn"));
+const SignUpPage = lazy(() => import("./pages/SignUp"));
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-500">Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <HomePage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <AboutPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <DashboardPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/project"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <ProjectPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <SignInPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <Suspense fallback={<p>Loading...</p>}>
+              <SignUpPage />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
