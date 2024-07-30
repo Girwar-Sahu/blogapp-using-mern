ed3787d3a6e7ace07c2c611c2a71e6ca114ed620
Girwar-Sahu <girvar786sahu@gmail.com>
2024-07-30 10:32:40 +0530

    connect mongodb database

--- api/index.js
+++ api/index.js
@@ -1,10 +1,31 @@
-import express from "express"
-const app = express()
+import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+dotenv.config()
 
-app.get('/',(req,res)=>{
-   res.send("hwllo world")
-})
+const app = express();
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 
-app.listen(5000,()=> {
-   console.log(`server running on port 5000`);
-})
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL,clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("mongodb connected");
+  } catch (err) {
+    console.log(err);
+  }
+  finally{
+    mongoose.disconnect()
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("hwllo world");
+});
+
+app.listen(5000,() => {
+  connect();
+  console.log(`server running on port 5000`);
+});
