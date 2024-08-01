47235aaf8c6be9ba6e3ea8f633ead04bfce4c28c
Girwar-Sahu <girvar786sahu@gmail.com>
2024-08-01 14:15:24 +0530

    add delete user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 import { varifyToken } from "../utils/varifyUser.js";
 const router = express.Router();
 
 router.put("/update/:userId", varifyToken, updateUser);
+router.delete("/delete/:userId", varifyToken, deleteUser);
 export default router;
