04f93858ca63b267c9bb1f8380b0a1f31f4e7def
Girwar-Sahu <girvar786sahu@gmail.com>
2024-08-02 17:17:14 +0530

    create get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { varifyToken } from "../utils/varifyUser.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getAllPost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", varifyToken, createPost);
-
+router.get("/getposts", getAllPost);
 export default router;
