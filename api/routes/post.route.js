6e500c7ea0ca671eb46af2ffa148041b49873c72
Girwar-Sahu <girvar786sahu@gmail.com>
2024-08-03 12:18:55 +0530

    create show user post inside admin dashboard

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,9 @@
 import express from "express";
 import { varifyToken } from "../utils/varifyUser.js";
-import { createPost, getAllPost } from "../controllers/post.controller.js";
+import { createPost, getPost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/create", varifyToken, createPost);
-router.get("/getposts", getAllPost);
+router.get("/getposts", getPost);
 export default router;
