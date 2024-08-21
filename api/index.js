974912a17c174d91bd52cf64603ecc8bf09ac8d7
Girwar-Sahu <girvar786sahu@gmail.com>
2024-08-21 16:57:40 +0530

    configure ports and domains

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true,
   optionSuccessStatus: 200,
 };
