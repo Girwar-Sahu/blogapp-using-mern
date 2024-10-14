968ce2d2b9c3a123996709ed68c6248f1f2d1f15
Girwar Sahu <109727463+Girwar-Sahu@users.noreply.github.com>
2024-10-14 06:08:03 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -28,7 +28,7 @@ function Home() {
   return (
     <div>
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
-        <h1 className="text-3xl font-bold lg:text-6xl ">Welcome to my blog</h1>
+        <h1 className="text-3xl font-bold lg:text-6xl ">Welcome to BlogiFy</h1>
         <p className="text-gray-500 tex-xs sm:text-sm">
           Here you'll find variety of articles and tutorials on topic such as
           web development, ai and ml, software engineering and programming
